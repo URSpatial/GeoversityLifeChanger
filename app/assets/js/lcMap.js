9216a49958f108d83b36f4a3f6d31a6b15936fb0
Nathan Strout <SpatialStrout@users.noreply.github.com>
2020-10-21 09:11:50 -0700

    cleanup

--- app/assets/js/lcMap.js
+++ app/assets/js/lcMap.js
@@ -1,14 +1,11 @@
 var mvpMap = {
+    sceneDiv: "mapView",
     scene: {},
     view: {},
-    // goTo: {},
-    // select: {},
     showTrail: null,
     showFacility: null
 
 }
-
-
 var coreLayerNames = {
     lcTrails: "Life Changer Trails",
     lcLodging: "Life Changer Lodging Facilities",
@@ -27,15 +24,13 @@ require([
         "dojo/on",
         "dojo/dom-style"
     ],
-    function(Map, WebScene, SceneView, ElevationLayer, BaseElevationLayer, domConstruct, dom, domClass, on, domStyle) {
+    function (Map, WebScene, SceneView, ElevationLayer, BaseElevationLayer, domConstruct, dom, domClass, on, domStyle) {
         var scene, view, layerViews, highlightedTrail, highlightedFacility;
         var showTrail, showFacility;
         initPage();
         initMap();
         mvpMap.scene = scene;
         mvpMap.view = view;
-        //mvpMap.goTo = goTo;
-        //mvpMap.select = select;
         mvpMap.showTrail = showTrail;
         mvpMap.showFacility = showFacility;
 
@@ -44,17 +39,13 @@ require([
             topoBtn.id = "baseTopo";
             topoBtn.className = "basemapButton"
             topoBtn.innerHTML = "Topographic";
-            document.getElementById("mapView").appendChild(topoBtn);
+            document.getElementById(mapView.sceneDiv).appendChild(topoBtn);
             var imgBtn = document.createElement("div");
             imgBtn.id = "baseImagery";
             imgBtn.className = "basemapButton"
             imgBtn.innerHTML = "Imagery";
-            document.getElementById("mapView").appendChild(imgBtn);
+            document.getElementById(mapView.sceneDiv).appendChild(imgBtn);
 
-            //         var toggleBase =domConstruct.create("div", {
-            //             id: "toggleBase"
-            //         }, "mapView");
-            // toggleBase.
         }
 
         function initMap() {
@@ -69,7 +60,7 @@ require([
                 }
             });
             view = new SceneView({
-                container: "mapView",
+                container: mapView.sceneDiv,
                 map: scene,
                 camera: {
                     heading: 2.157,
@@ -91,53 +82,45 @@ require([
                     }
                 }
             });
-
-            view.when(function() {
+            view.when(function () {
                 setBasemap("topo");
                 var baseTopo = dom.byId("baseTopo");
                 var baseImagery = dom.byId("baseImagery");
-                on(baseTopo, "click", function(evt) {
+                on(baseTopo, "click", function (evt) {
                     setBasemap("topo");
 
                 });
-                on(baseImagery, "click", function(evt) {
+                on(baseImagery, "click", function (evt) {
                     setBasemap("imagery");
-
                 });
 
-                view.on("layerview-create", function(event) {
-                    //layerViews[event.layer.id] = event.layerView;
+                view.on("layerview-create", function (event) {
+                    //
                 });
             });
-            scene.when(function() {
-
-
+            scene.when(function () {
                 var coreLayerProps = Object.keys(coreLayerNames);
                 for (i = 0; i < coreLayerProps.length; i++) {
                     var lyrName = coreLayerNames[coreLayerProps[i]];
                     var lyr = getLayer(lyrName);
-                    view.whenLayerView(lyr).then(function(layerView) {
+                    view.whenLayerView(lyr).then(function (layerView) {
                         layerViews[layerView.layer.title] = layerView;
                     })
 
                 }
-
-
-
-
             });
 
-            showTrail = function(id, highlight = true, zoom = true, popup = null) {
+            showTrail = function (id, highlight = true, zoom = true, popup = null) {
                 var targetLayer;
                 var whereClause;
 
                 targetLayer = layerViews["Mamoni Valley Trails"];
-                whereClause = "Name = '" + id + "'";
+                //whereClause = "Name = '" + id + "'";
                 whereClause = "TrailID = '" + id + "'";
                 targetLayer.queryFeatures({
                     where: whereClause,
                     returnGeometry: true
-                }).then(function(results) {
+                }).then(function (results) {
                     if (highlightedTrail) {
                         highlightedTrail.remove();
                     }
@@ -169,17 +152,17 @@ require([
                 })
 
             }
-            showFacility = function(id, highlight = true, zoom = true, popup = null) {
+            showFacility = function (id, highlight = true, zoom = true, popup = null) {
                 var targetLayer;
                 var whereClause;
 
                 targetLayer = layerViews["Life Changer Lodging Facilities"];
-                whereClause = "Name = '" + id + "'";
+                //whereClause = "Name = '" + id + "'";
                 whereClause = "FacilityID = '" + id + "'";
                 targetLayer.queryFeatures({
                     where: whereClause,
                     returnGeometry: true
-                }).then(function(results) {
+                }).then(function (results) {
                     if (highlightedFacility) {
                         highlightedFacility.remove();
                     }
@@ -214,21 +197,17 @@ require([
 
         function setBasemap(basemap) {
             if (basemap == "topo") {
-
                 getLayer("Mamoni Terrain").visible = true;
                 getLayer("World Hillshade").visible = true;
                 getLayer("World Terrain Base").visible = true;
-                //getLayer("Pansharpened Satellite Imagery - Feb 7, 2011 (GeoEye DigitalGlobe)").visible = false;
                 getLayer("Mamoni Drone Imagery (2017-2019)").visible = false;
                 getLayer("World Imagery").visible = false;
                 domClass.add("baseTopo", "active");
                 domClass.remove("baseImagery", "active");
-
             } else if (basemap == "imagery") {
                 getLayer("Mamoni Terrain").visible = false;
                 getLayer("World Hillshade").visible = false;
                 getLayer("World Terrain Base").visible = false;
-                //getLayer("Pansharpened Satellite Imagery - Feb 7, 2011 (GeoEye DigitalGlobe)").visible = false;
                 getLayer("Mamoni Drone Imagery (2017-2019)").visible = true;
                 getLayer("World Imagery").visible = true;
                 domClass.remove("baseTopo", "active");
